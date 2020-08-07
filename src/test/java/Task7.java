44db44511c666bfc084b84982382be118fb65049
ashabanyan <69008330+ashabanyan@users.noreply.github.com>
2020-08-08 01:40:27 +0300

    Update Task7.java

--- src/test/java/Task7.java
+++ src/test/java/Task7.java
@@ -4,14 +4,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
-import java.util.concurrent.TimeUnit;
 
 public class Task7 {
-
     private WebDriver driver;
     private WebDriverWait wait;
+
 
     @Before
     public void start() {
@@ -21,19 +23,34 @@ public class Task7 {
         driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@name=\"login\"]")).click();
+
     }
 
     @Test
     public void test1() {
-        driver.findElement(By.xpath(".//span[text()='Appearence']/..")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Assert.assertNotNull(driver.findElement(By.id("h1")).getText());
+        driver.findElement(By.xpath(".//ul[@id='box-apps-menu']/li[1]/a")).click();
+        wait = new WebDriverWait(driver, 10);
+        driver.findElement(By.xpath(".//li[@id='doc-template']/a")).click();
+        WebElement element = wait.until(presenceOfElementLocated(By.xpath("//h1")));
+        Assert.assertNotNull(driver.findElement(By.xpath("//h1")).getText());
+    }
+
+    @Test
+    public void test2() {
+        driver.findElement(By.xpath("//ul[@id='box-apps-menu']/li[1]/a")).click();
+        wait = new WebDriverWait(driver, 10);
+        driver.findElement(By.xpath(".//li[@id='doc-logotype']/a")).click();
+        WebElement element = wait.until(presenceOfElementLocated(By.xpath("//h1")));
+        Assert.assertNotNull(driver.findElement(By.xpath("//h1")).getText());
     }
 
     @After
-    public void stop() {
+    public void finish() {
         driver.quit();
-        driver = null;
     }
+
+
+
+
 
 }
