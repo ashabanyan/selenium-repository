e7f2a551ab00c34b9119d46330043f30a3a71c07
Alexander Shabanyan <ashabanyan.dev@gmail.com>
2020-08-11 18:54:34 +0300

    Task8_finished + other

--- src/test/java/Task7.java
+++ src/test/java/Task7.java
@@ -3,31 +3,23 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
-public class Task7_stickers {
+
+public class Task7 {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    private boolean isElementPresent(String css) {
-        try {
-            driver.findElement(By.cssSelector(css));
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-        return true;
-    }
 
     @Before
     public void start() {
         driver = BrowserProperties.getProperty("firefox");
         wait = new WebDriverWait(driver, 10);
-        driver.get("http://localhost/litecart");
+        driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@name=\"login\"]")).click();
@@ -35,12 +27,22 @@ public class Task7_stickers {
 
     @Test
     public void test() {
-        int size = driver.findElements(By.xpath("//li[@class='product column shadow hover-light']"));
-        for (int i = 0; i<size; i++) {
-            driver.findElements(By.cssSelector("[class^=sticker"));
+        int par = driver.findElements(By.cssSelector("li#app-")).size();
+        for (int i = 0; i<par; i++) {
+            driver.findElements(By.cssSelector("li#app-")).get(i).click();
+            for (int j = 0; j<driver.findElements(By.cssSelector("[id^=doc-")).size(); j++) {
+                driver.findElements(By.cssSelector("[id^=doc-")).get(j).click();
+                driver.findElement(By.cssSelector("h1"));
+                System.out.println(driver.findElement(By.cssSelector("h1")).getText());
+            }
         }
     }
 
+    @After
+    public void finish() {
+        driver.quit();
+    }
+
+
 
 }
-
--- src/test/java/Task8.java
+++ src/test/java/Task8.java
@@ -3,46 +3,37 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
-
-public class Task7_new {
+public class Task8 {
     private WebDriver driver;
     private WebDriverWait wait;
-
 
     @Before
     public void start() {
         driver = BrowserProperties.getProperty("firefox");
         wait = new WebDriverWait(driver, 10);
-        driver.get("http://localhost/litecart/admin/");
-        driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("admin");
+        driver.get("http://localhost/litecart");
+        /*driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("admin");
-        driver.findElement(By.xpath("//button[@name=\"login\"]")).click();
+        driver.findElement(By.xpath("//button[@name=\"login\"]")).click();*/
     }
 
     @Test
     public void test() {
-        int par = driver.findElements(By.cssSelector("li#app-")).size();
-        for (int i = 0; i<par; i++) {
-            driver.findElements(By.cssSelector("li#app-")).get(i).click();
-            for (int j = 0; j<driver.findElements(By.cssSelector("[id^=doc-")).size(); j++) {
-                driver.findElements(By.cssSelector("[id^=doc-")).get(j).click();
-                driver.findElement(By.cssSelector("h1"));
-                System.out.println(driver.findElement(By.cssSelector("h1")).getText());
-            }
-        }
+        int ducks= driver.findElements(By.xpath("//li[@class='product column shadow hover-light']")).size();
+        int stickers = driver.findElements(By.cssSelector("[class^=sticker")).size();
+        Assert.assertEquals(ducks,stickers);
     }
 
     @After
     public void finish() {
         driver.quit();
     }
-
-
-
 }
+
