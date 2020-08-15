122cd766522ccb22d9bafd24b29a6d66a20b6d63
Alexander Shabanyan <ashabanyan.dev@gmail.com>
2020-08-16 01:40:53 +0300

    Create Task11.java

---
+++ src/test/java/Task11.java
@@ -0,0 +1,52 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Task11 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = BrowserProperties.getProperty("firefox");
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void test1() {
+        driver.get("https://litecart.stqa.ru/en/");
+        driver.findElement(By.cssSelector("[name=login_form] [href]")).click();
+
+        driver.findElement(By.cssSelector("[name=firstname]")).sendKeys("Александр");
+        driver.findElement(By.cssSelector("[name=lastname]")).sendKeys("Шабанян");
+        driver.findElement(By.cssSelector("[name=address1]")).sendKeys("г.Москва, улица Мира, дом 128, кв. 314");
+        driver.findElement(By.cssSelector("[name=postcode]")).sendKeys("123456");
+        driver.findElement(By.cssSelector("[name=city]")).sendKeys("Los-Angeles");
+
+
+        Select country = new Select(driver.findElement(By.xpath("//select[@name='country_code']")));
+        country.selectByVisibleText("United States");
+
+//        String email = "test2@gmail.com";
+//        driver.findElement(By.cssSelector("[name=email]")).sendKeys(email);
+//        driver.findElement(By.cssSelector("[name=phone]")).sendKeys("89033583958");
+//        String password = "123123123";
+//        driver.findElement(By.cssSelector("[name=password]")).sendKeys(password);
+//        driver.findElement(By.cssSelector("[name=confirmed_password]")).sendKeys(password);
+//        driver.findElement(By.cssSelector("[name=create_account]")).click();
+//
+//        driver.findElement(By.xpath("//ul[@class='list-vertical']//li[5]/a")).click();
+//
+//        driver.findElement(By.cssSelector("[name=email]")).sendKeys(email);
+//        driver.findElement(By.cssSelector("[name=password]")).sendKeys(password);
+//        driver.findElement(By.cssSelector("[name=login]")).click();
+//
+//        driver.findElement(By.xpath("//ul[@class='list-vertical']//li[5]/a")).click();
+
+    }
+
+}
