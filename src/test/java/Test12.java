ea6219fc5926aefa9915b8f54182ce232eeff98f
Alexander Shabanyan <ashabanyan.dev@gmail.com>
2020-08-17 00:53:24 +0300

    Test12 not_finished

--- src/test/java/Test12.java
+++ src/test/java/Test12.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Test12 {
     private WebDriver driver;
     private WebDriverWait wait;
+    private String path = getClass().getClassLoader().getResource(".").getPath();
+    private String Imagepath = "D:\\Education\\Testing\\Selenium WebDriver\\selenium-repository\\target\\test-classes\\testduck.jpeg";
 
     @Before
     public void start() {
@@ -22,6 +24,20 @@ public class Test12 {
     public void test() {
         driver.findElement(By.xpath(".//span[contains(text(),\"Catalog\")]/..")).click();
         driver.findElement(By.xpath("//div[@style='float: right;']/a[2]")).click();
+
+        driver.findElement(By.xpath("//div[@id='tab-general']//tr[1]//input[@value='1']")).click();
+        driver.findElement(By.xpath("//input[@name='name[en]']")).sendKeys("Test duck");
+        driver.findElement(By.xpath("//input[@name='code']")).sendKeys("TEST01");
+        driver.findElement(By.xpath("//td[contains(text(), 'Male')]/..//input")).click();
+
+        driver.findElement(By.xpath("//input[@name='quantity']")).clear();
+        driver.findElement(By.xpath("//input[@name='quantity']")).sendKeys("1");
+
+        driver.findElement(By.xpath("//input[@name='new_images[]']")).sendKeys(Imagepath);
+
+
+
+        System.out.println(path);
     }
 
 
