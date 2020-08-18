ee5427d7cca8eacda494ff1c1c44ffbee3ff2a09
Alexander Shabanyan <ashabanyan.dev@gmail.com>
2020-08-18 19:39:50 +0300

    Update Task10.java

--- src/test/java/Task10.java
+++ src/test/java/Task10.java
@@ -14,7 +14,7 @@ public class Task10 {
 
     @Before
     public void start() {
-        driver = BrowserProperties.getProperty("firefox");
+        driver = BrowserProperties.getProperty("chrome");
         wait = new WebDriverWait(driver, 10);
     }
 
@@ -22,11 +22,8 @@ public class Task10 {
     public void test() {
         driver.get("http://localhost/litecart");
 
-        String mainPriseColor=driver.findElement(By.xpath("//div[@id='box-campaigns']//s[@class='regular-price']")).getCssValue("color");
-        String[] color=mainPriseColor.split(",");
-        System.out.println(color[0].substring(4));
-        System.out.println("-----------------------------------");
-        System.out.println("-----------------------------------");
+
+
 
         int campaigns_size = driver.findElements(By.cssSelector("[id=box-campaigns]")).size();
         for (int i =0; i<campaigns_size; i++) {
@@ -35,8 +32,10 @@ public class Task10 {
             String outregprice = driver.findElements(By.xpath("//div[@id='box-campaigns']//li[@class='product column shadow hover-light']//div[@class='price-wrapper']")).get(i).findElement(By.cssSelector("[class=regular-price]")).getText();
             String outsaleprice = driver.findElements(By.xpath("//div[@id='box-campaigns']//li[@class='product column shadow hover-light']//div[@class='price-wrapper']")).get(i).findElement(By.cssSelector("[class=campaign-price]")).getText();
             String outregcolor = driver.findElement(By.cssSelector("[id=box-campaigns] [class=regular-price]")).getCssValue("color");
+            String[] coloroutreg = outregcolor.split(",");
             String outregthrough = driver.findElement(By.cssSelector("[id=box-campaigns] [class=regular-price]")).getCssValue("text-decoration-line");
             String outsalecolor = driver.findElement(By.cssSelector("[id=box-campaigns] [class=campaign-price]")).getCssValue("color");
+            String[] coloroutsale = outsalecolor.split(",");
             String outsalestrong = driver.findElements(By.xpath("//div[@id='box-campaigns']//li[@class='product column shadow hover-light']//strong")).get(i).getAttribute("class");
             String outregsize = driver.findElement(By.cssSelector("[id=box-campaigns] [class=regular-price]")).getCssValue("font-size");
             String outsalesize = driver.findElement(By.cssSelector("[id=box-campaigns] [class=campaign-price]")).getCssValue("font-size");
@@ -49,8 +48,10 @@ public class Task10 {
             String inregprice = driver.findElement(By.cssSelector("[class=content] [class=price-wrapper] [class=regular-price]")).getText();
             String insaleprice = driver.findElement(By.cssSelector("[class=content] [class=price-wrapper] [class=campaign-price]")).getText();
             String inregcolor = driver.findElement(By.cssSelector("[class=content] [class=regular-price]")).getCssValue("color");
+            String[] colorinreg = inregcolor.split(",");
             String inregthrough = driver.findElement(By.cssSelector("[class=content] [class=regular-price]")).getCssValue("text-decoration-line");
             String insalecolor = driver.findElement(By.cssSelector("[class=content] [class=campaign-price]")).getCssValue("color");
+            String[] colorinsale = insalecolor.split(",");
             String insalestrong = driver.findElements(By.xpath("//div[@class='content']//div[@class='information']//div[@class='price-wrapper']/strong")).get(i).getAttribute("class");
             String inregsize = driver.findElement(By.cssSelector("[class=content] [class=regular-price]")).getCssValue("font-size");
             String insalesize = driver.findElement(By.cssSelector("[class=content] [class=campaign-price]")).getCssValue("font-size");
@@ -68,19 +69,19 @@ public class Task10 {
                 System.out.println("Акционные цены совпадают: " + outsaleprice + " & " + insaleprice);
             }
             System.out.println("--------------------------------------------");
-            if (outregcolor.equals("rgb(119, 119, 119)") & outregthrough.equals("line-through")) {
+            if ((coloroutreg[0].substring(5).equals(coloroutreg[1].substring(1))) && (coloroutreg[1].substring(1).equals(coloroutreg[2].substring(1))) & outregthrough.equals("line-through")) {
                 System.out.println("Регулярная цена на главной странице отображена в сером цвете и перечеркнута");
             }
             System.out.println("--------------------------------------------");
-            if (outsalecolor.equals("rgb(204, 0, 0)") & outsalestrong.equals("campaign-price")) {
+            if ((coloroutsale[1].substring(1).equals("0")) && (coloroutsale[2].substring(1).equals("0")) & outsalestrong.equals("campaign-price")) {
                 System.out.println("Акционная цена на главной странице отображена в красном цвете и в жирном шрифте!");
             }
             System.out.println("--------------------------------------------");
-            if (inregcolor.equals("rgb(102, 102, 102)") & inregthrough.equals("line-through")) {
+            if ((colorinreg[0].substring(5).equals(colorinreg[1].substring(1))) && (colorinreg[1].substring(1).equals(colorinreg[2].substring(1))) & inregthrough.equals("line-through")) {
                 System.out.println("Регулярная цена в карточке товара отображена в сером цвете и перечеркнута");
             }
             System.out.println("--------------------------------------------");
-            if (insalecolor.equals("rgb(204, 0, 0)") & insalestrong.equals("campaign-price")) {
+            if ((colorinsale[1].substring(1).equals("0")) && (colorinsale[2].substring(1).equals("0")) & insalestrong.equals("campaign-price")) {
                 System.out.println("Акционная цена в карточке товара отображена в красном цвете и в жирном шрифте!");
             }
             System.out.println("--------------------------------------------");
